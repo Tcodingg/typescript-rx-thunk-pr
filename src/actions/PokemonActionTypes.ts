178749e7f6bfb61bdd4360512eb8b10bd8556378
DePrincio <princetlive@yahoo.com>
2021-07-15 13:02:10 -0400

    add more action types

--- src/actions/PokemonActionTypes.ts
+++ src/actions/PokemonActionTypes.ts
@@ -3,13 +3,40 @@ export const POKEMON_LOADING = 'POKEMON_LOADING';
 export const POKEMON_FAIL = 'POKEMON_FAIL';
 export const POKEMON_SUCCESS = 'POKEMON_SUCCESS';
 
-interface PokemonLoading {
+export type PokemonAbility = {
+	ability: {
+		name: string;
+		url: string;
+	};
+};
+
+export type PokemonSprites = {
+	front_default: string;
+};
+
+export type PokeymonState = {
+	base_state: number;
+	stat: {
+		name: string;
+	};
+};
+
+export interface PokemonLoading {
 	type: typeof POKEMON_LOADING;
 }
-interface PokemonFail {
+export interface PokemonFail {
 	type: typeof POKEMON_FAIL;
 }
-interface PokemonSuccess {
+export interface PokemonSuccess {
 	type: typeof POKEMON_SUCCESS;
-	payload: {};
+	payload: {
+		Abilities: PokemonSprites[];
+		sprites: PokemonSprites;
+		stats: PokeymonState[];
+	};
 }
+
+export type PokemonDispatchTypes =
+	| PokemonLoading
+	| PokemonFail
+	| PokemonSuccess;
