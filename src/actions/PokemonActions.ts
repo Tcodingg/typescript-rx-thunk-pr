f03eb3063794265cad6745f031abafdf813f1b5b
DePrincio <princetlive@yahoo.com>
2021-07-20 10:27:11 -0400

    setup loading, success and fail actions

--- src/actions/PokemonActions.ts
+++ src/actions/PokemonActions.ts
@@ -1,7 +1,26 @@
 import { Dispatch } from 'redux';
+import axios from 'axios';
+import {
+	PokemonDispatchTypes,
+	POKEMON_SUCCESS,
+	POKEMON_FAIL,
+	POKEMON_LOADING,
+} from './PokemonActionTypes';
 
-const getPokemon = () => (dispatch: Dispatch) => {
-	try {
-		dispatch({ type: 'LOADING_POKEMON' });
-	} catch (error) {}
-};
+const getPokemon =
+	(pokemon: string) => async (dispatch: Dispatch<PokemonDispatchTypes>) => {
+		try {
+			dispatch({ type: POKEMON_LOADING });
+			const res = await axios.get(
+				`https://pokeapi.co/api/v2/pokemon/${pokemon}`
+			);
+			dispatch({
+				type: POKEMON_SUCCESS,
+				payload: res.data,
+			});
+		} catch (error) {
+			dispatch({
+				type: POKEMON_FAIL,
+			});
+		}
+	};
